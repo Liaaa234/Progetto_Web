2e2d077a10f417cf9d617177f8963dbfbb01a740
Emanuele Meleddu <emanuelemeleddu@gmail.com>
2026-06-20 16:32:26 +0200

    Inserita docstring per get_users

--- app/models/event.py
+++ app/models/event.py
@@ -2,7 +2,7 @@ from sqlmodel import Field, SQLModel
 from datetime import datetime
 
 class Event(SQLModel, table=True):
-    event_id: int | None = Field(default=None, primary_key=True)#generato automaticamente dal database
+    id: int | None = Field(default=None, primary_key=True)#generato automaticamente dal database
     title: str
     description: str
     date: datetime
