7ffe5965cb26c61114e02f522205425a9443cbca
glendatzori <glendatzori@gmail.com>
2026-06-22 15:45:59 +0200

    Creata router registrations

---
+++ app/routers/registrations.py
@@ -0,0 +1,11 @@
+from fastapi import APIRouter
+from sqlmodel import select  #Importa la funzione select dalla libreria SQLModel
+
+from app.models.registration import Registration  #Importa il modello Registration dal modulo app.models.registration
+from app.data.db import SessionDep  #importa SessionDep dal file della configurazione del database
+
+#Inizializzo un'istanza APIRouter e la salvo nella variabile router
+router = APIRouter(
+    prefix="/registrations",
+    tags=["registrations"]
+)
