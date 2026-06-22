00fd5219ed998518c27dd8c76945ab0a4869a6b0
Liaaa234 <giu.gt04@gmail.com>
2026-06-22 16:48:04 +0200

    aggiunto commento

--- app/models/event.py
+++ app/models/event.py
@@ -1,8 +1,10 @@
+#STRUTTURA DATI PER L'OGGETTO EVENTO
+
 from sqlmodel import Field, SQLModel
 from datetime import datetime
 
 class Event(SQLModel, table=True):
-    id: int | None = Field(default=None, primary_key=True)#generato automaticamente dal database
+    id: int | None = Field(default=None, primary_key=True)  #generato automaticamente dal database
     title: str
     description: str
     date: datetime
