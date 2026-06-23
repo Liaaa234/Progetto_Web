9d3a49c4fd799043e499e31151fddf35a69955f6
Liaaa234 <giu.gt04@gmail.com>
2026-06-23 23:03:50 +0200

    aggiunto spazi tra le classi

--- app/models/user.py
+++ app/models/user.py
@@ -5,11 +5,14 @@ class User(SQLModel):#classe base dei modelli del database
     username:str
     name: str
     email:str
+
 class CreateUser(User):
     pass
+
 class UserDB(User, table=True):#classe che rappresenta la tabella del database
     __tablename__ = "user"#nome classe nel database SQL
     username:str=Field(default=None, primary_key=True)
+
 class UserPublic(User):
     pass
 
