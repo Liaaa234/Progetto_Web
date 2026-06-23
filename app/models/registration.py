87d3a8f802653011b09d05de2ac63fd43cfe4d57
glendatzori <glendatzori@gmail.com>
2026-06-23 22:52:15 +0200

    sistemazione codice registrations

--- app/models/registration.py
+++ app/models/registration.py
@@ -4,11 +4,3 @@ class Registration(SQLModel, table=True):
     username: str = Field(primary_key=True, foreign_key="user.username")
     event_id: int = Field(primary_key=True, foreign_key="event.id")
 
-# struttura dati per Registration
-
-class Registration(SQLModel):     # attributi comuni
-    username: str
-    event_id: int
-
-class RegistrationPublic(Registration):   # usata nelle GET
-    pass
